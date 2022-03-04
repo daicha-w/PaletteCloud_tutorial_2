afc93c002b3d9ff44cbad3021b15b6906257eaa4
daicha-w <58203483+daicha-w@users.noreply.github.com>
2022-03-04 16:48:37 +0000

    Introduce slim

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
+    config.generators.template_engine = :slim
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
