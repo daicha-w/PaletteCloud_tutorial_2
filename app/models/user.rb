46a81aec14a066c7ee7a020cb264a73ce5f40f42
daicha-w <58203483+daicha-w@users.noreply.github.com>
2022-04-07 04:40:51 +0000

    Explicit class_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_many :microposts, dependent: :destroy
+  has_many :microposts, dependent: :destroy, class_name: '::Micropost'
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :following, through: :active_relationships,  source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
+  has_many :following, through: :active_relationships,  source: :followed, class_name: 'User'
+  has_many :followers, through: :passive_relationships, source: :follower, class_name: 'User'
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_save   :downcase_unique_name
